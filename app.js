4657ea92bea5af4f41bb1c294d4af5fe25672e58
Jacz <jacknagle22@gmail.com>
2017-12-18 06:18:34 +1100

    Update app.js meh

--- app.js
+++ app.js
@@ -9,28 +9,16 @@ app.get("/", (req, res) => {
     res.render("home");
 });
 
-// app.get("/commands", (req, res) => {
-// res.render("home");
-// });
-
-app.get("/donate", (req, res) => {
-    res.render("donate");
-});
+app.get("/donate", (req, res) => res.render("donate"));
 
 app.get("/invite", (req, res) => {
     res.redirect("https://discordapp.com/oauth2/authorize?client_id=303181184718995457&scope=bot&permissions=372763718");
 });
 
-app.get("/support", (req, res) => {
-    res.redirect("https://discord.gg/NQCTdEc");
-});
+app.get("/support", (req, res) => res.redirect("https://discord.gg/NQCTdEc"));
 
-app.get("/aditya", (req, res) => {
-    res.redirect("https://www.adityatd.me")
-});
+app.get("/aditya", (req, res) => res.redirect("https://www.adityatd.me"));
 
-app.get("/jacz", (req, res) => {
-    res.redirect("http://jaczaus.me")
-});
+app.get("/jacz", (req, res) => res.redirect("http://jaczaus.me"));
 
 app.listen(4321, () => console.log(`PenguBot website running on port 4321.`));
