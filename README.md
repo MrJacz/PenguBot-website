31c799b9e92ed4eaedb8cf1bf996dd2de9ebaf0b
Jack N <jacknagle22@gmail.com>
2018-02-08 23:32:04 +1100

    Clear branch

--- README.md
+++
@@ -1,2 +0,0 @@
-# PenguBot website
-The repo for PenguBot's website this is not to be used and is under "All Rights Reserved"
--- js/main.js
+++
@@ -1,8 +0,0 @@
-$().ready(() => {
-    const $burger = $(".navbar-burger");
-    const $menu = $(".navbar-menu");
-    $burger.click(() => {
-        $burger.toggleClass("is-active");
-        $menu.toggleClass("is-active");
-    });
-});
