4d29426986f15d410d0817e6f406d2d7f59c3208
Travis CI <>
2018-02-10 21:59:37 +0000

    Docs build: e679df5fb3f589e0981bb4f7931eb1ca4872c474

--- js/stats.js
+++ js/stats.js
@@ -1,18 +1,25 @@
 $().ready(() => {
     toggleBurger();
     stats();
-    listShards();
     setInterval(stats, 60000);
 });
 
-function listShards() {
-    $.get("https://api.pengubot.com/stats/shards").done(shards => {
-        for (const id in shards) {
-            const shard = shards[id];
+
+function stats() {
+    $.get("https://api.pengubot.com/stats").done(data => {
+        $("#guildCount").text(data.guilds.toLocaleString());
+        $("#channelCount").text(data.channels.toLocaleString());
+        $("#userCount").text(data.users.toLocaleString());
+        $("#memoryCount").text(`${data.memory.toFixed(2)} MB`);
+        $("#voiceCount").text(data.vcs.toLocaleString());
+        $("#cmdCount").text(data.cmdRun);
+
+        for (let i = 0; i < data.shards.length; i++) {
+            const shard = data.shards[i];
             $("#shardColumns").append(`
         <div class="card">
         <header class="card-header">
-          <p class="card-header-title"> ${id.replace("shard_", "Shard ")} </p>
+          <p class="card-header-title"> Shard ${shard.shard} </p>
         </header>
         <div class="card-content">
           <div class="content">
@@ -22,30 +29,16 @@ function listShards() {
             <p> Memory: <strong> ${shard.memory.toFixed(2)} MB </strong> </p>
             <p> Voice Connections: <strong> ${shard.vcs.toLocaleString()} </strong> </p>
             <p> Uptime: <strong> ${duration(shard.uptime)} </strong> </p>
+            <p> Ping: <strong> ${Math.round(shard.ping)} ms </strong> </p>
             <p> Commands Ran: <strong> ${shard.cmdRun} </strong> </p>
           </div>
         </div>
       </div>
-`);
+      `);
         }
     });
 }
 
-function stats() {
-    const guildText = $("#guildCount");
-    const channelText = $("#channelCount");
-    const userText = $("#userCount");
-    const memoryText = $("#memoryCount");
-    const voiceText = $("#voiceCount");
-
-    $.get("https://api.pengubot.com/stats").done(data => {
-        guildText.text(data.guilds.toLocaleString());
-        channelText.text(data.channels.toLocaleString());
-        userText.text(data.users.toLocaleString());
-        memoryText.text(`${data.memory.toFixed(2)} MB`);
-        voiceText.text(data.vcs.toLocaleString());
-    });
-}
 
 function toggleBurger() {
     const $burger = $(".navbar-burger");
