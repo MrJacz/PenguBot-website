ab7046a18cecc3251ffa4e7fa024bc3ba9eb0c9f
Jack N <jacknagle22@gmail.com>
2018-02-12 17:26:23 +1100

    Just casual updates:tm:

--- src/js/stats.js
+++ src/js/stats.js
@@ -1,4 +1,4 @@
-$().ready(() => {
+$(document).ready(() => {
     toggleBurger();
     stats();
     setInterval(stats, 60000);
@@ -7,38 +7,45 @@ $().ready(() => {
 
 function stats() {
     $.get("https://api.pengubot.com/stats").done(data => {
-        $("#guildCount").text(data.guilds.toLocaleString());
-        $("#channelCount").text(data.channels.toLocaleString());
-        $("#userCount").text(data.users.toLocaleString());
-        $("#memoryCount").text(`${data.memory.toFixed(2)} MB`);
-        $("#voiceCount").text(data.vcs.toLocaleString());
-        $("#cmdCount").text(data.cmdRun);
-        $("#shardColumns").html("");
-        for (let i = 0; i < data.shards.length; i++) {
-            const shard = data.shards[i];
-            $("#shardColumns").append(`
-        <div class="card">
-        <header class="card-header">
-          <p class="card-header-title"> Shard ${shard.shard} </p>
-        </header>
-        <div class="card-content">
-          <div class="content">
-            <p> Guilds: <strong> ${shard.guilds.toLocaleString()} </strong> </p>
-            <p> Channels: <strong> ${shard.channels.toLocaleString()} </strong> </p>
-            <p> Users: <strong> ${shard.users.toLocaleString()} </strong> </p>
-            <p> Memory: <strong> ${shard.memory.toFixed(2)} MB </strong> </p>
-            <p> Voice Connections: <strong> ${shard.vcs.toLocaleString()} </strong> </p>
-            <p> Uptime: <strong> ${duration(shard.uptime)} </strong> </p>
-            <p> Ping: <strong> ${Math.round(shard.ping)} ms </strong> </p>
-            <p> Commands Ran: <strong> ${shard.cmdRun} </strong> </p>
-          </div>
-        </div>
-      </div>
-      `);
-        }
+        updateStats(data);
+        listShards(data.shards);
     });
 }
 
+function updateStats(data) {
+    $("#guildCount").text(data.guilds.toLocaleString());
+    $("#channelCount").text(data.channels.toLocaleString());
+    $("#userCount").text(data.users.toLocaleString());
+    $("#memoryCount").text(`${data.memory.toFixed(2)} MB`);
+    $("#voiceCount").text(data.vcs.toLocaleString());
+    $("#cmdCount").text(data.cmdRun.toLocaleString());
+}
+
+function listShards(shards) {
+    $("#shardColumns").html("");
+    for (let i = 0; i < shards.length; i++) {
+        const shard = shards[i];
+        $("#shardColumns").append(`
+    <div class="card">
+    <header class="card-header">
+      <p class="card-header-title"> Shard ${shard.shard} </p>
+    </header>
+    <div class="card-content">
+      <div class="content">
+        <p> Guilds: <strong> ${shard.guilds.toLocaleString()} </strong> </p>
+        <p> Channels: <strong> ${shard.channels.toLocaleString()} </strong> </p>
+        <p> Users: <strong> ${shard.users.toLocaleString()} </strong> </p>
+        <p> Memory: <strong> ${shard.memory.toFixed(2)} MB </strong> </p>
+        <p> Voice Connections: <strong> ${shard.vcs.toLocaleString()} </strong> </p>
+        <p> Uptime: <strong> ${duration(shard.uptime)} </strong> </p>
+        <p> Ping: <strong> ${Math.round(shard.ping)} ms </strong> </p>
+        <p> Commands Ran: <strong> ${shard.cmdRun} </strong> </p>
+      </div>
+    </div>
+  </div>
+  `);
+    }
+}
 
 function toggleBurger() {
     const $burger = $(".navbar-burger");
